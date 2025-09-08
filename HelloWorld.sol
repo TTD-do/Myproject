3df678d52deb8ecb933307a44fa4318cbe8d0dff
TTD-do <160862089+TTD-do@users.noreply.github.com>
2025-09-08 18:53:51 +0700

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor(string memory _msg) {
+        message = _msg;
+    }
+
+    function setMessage(string memory _msg) public {
+        message = _msg;
+    }
+}
